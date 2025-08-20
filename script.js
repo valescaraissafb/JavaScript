b9aa5150d92b4bb1e1467f6afe0d7948ff0839f9
valescaraissafb <valescaraissafb@gmail.com>
2025-08-20 19:36:12 +0000

    primeiro commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/*let x = 0.3 + 0.5;
+console.log(x);
+let y = 0.1 + 0.2;
+console.log(y);*/ 
+
+/*let a = undefined
+console.log(a);*/
+
+/*let nome = ("Valesca")
+console.log(nome)
+let idade = ('27')
+console.log(idade)
+ 
+let cidade = "Recife"
+let estado = "Pernambuco"
+console.log ("Moro em " + cidade+ "/" +estado) */
+
+
+/*let numero1 = 10
+let numero2 = 5
+console.log ("Valor da soma ", numero1 + numero2)
+console.log ("Valor da subtração ", numero1-numero2)
+console.log ("Valor da multiplicação ", numero1 * numero2); 
+console.log ("Valor da divisão ", numero1 / numero2); */
+
+/*let preço = "49,90";
+console.log (preço);
+preço = Number (49.90);
+console.log(preço); */
+
+/*let total = 0.1 + 0.2 
+console.log ("Total sem arredondar:" ,total);
+ 
+let totalArredondado = total.toFixed(2)
+console.log("Total arredondado; " ,totalArredondado);*/
+
+/*let mensagem = ("Olá Mundo!")
+console.log("Em maiúsculo: " + mensagem.toUpperCase());
+console.log("Em minúscula: " + mensagem.toLowerCase());*/
+
+let palavra = "JavaScript"
+console.log("Primeira letra: " + palavra[0]); // 
+
+
